b1dc1106c57502d56f239506281e481d16bdeba3
Philipp Classen <philipp.classen@gmx.net>
2012-04-05 12:02:28 +0200

    Fixed bugs in the statistical learning

--- app/models/game_history.rb
+++ app/models/game_history.rb
@@ -6,7 +6,7 @@ class GameHistory < ActiveRecord::Base
 
     board.moves.each do |m|
       move = { :row => m[0], :column => m[1] }
-      computer_on_move = tmp_board.next_to_move? == computer_on_move
+      computer_on_move = tmp_board.next_to_move? == computer_side
       tmp_board.make_move(move)
       packed_board = tmp_board.pack_board
       
@@ -46,17 +46,32 @@ class GameHistory < ActiveRecord::Base
     best_score = nil
     best_moves = []
 
-    computer_on_move = board.next_to_move? == computer_on_move
     moves.each do |m|
       stats = where(:board_size       => board.board_size,
                     :row_length       => board.row_length,
                     :board            => packed_boards[m],
-                    :computer_on_move => computer_on_move).first
+                    :computer_on_move => true).first
       if stats
-        score = compute_score(stats.wins, stats.losses, stats.draws)
+        own_wins, own_losses, own_draws = stats.wins, stats.losses, stats.draws
       else
-        score = compute_score(0, 0, 0)
+        own_wins, own_losses, own_draws = 0, 0, 0
       end
+
+      enemy_stats = where(:board_size       => board.board_size,
+                          :row_length       => board.row_length,
+                          :board            => packed_boards[m],
+                          :computer_on_move => false).first
+      if enemy_stats
+        enemy_wins   = enemy_stats.losses
+        enemy_losses = enemy_stats.wins
+        enemy_draws  = enemy_stats.draws
+      else
+        enemy_wins, enemy_losses, enemy_draws = 0, 0, 0
+      end
+
+      score = compute_score(own_wins, own_losses, own_draws,
+                            enemy_wins, enemy_losses, enemy_draws)
+
       if best_score == nil || score >= best_score
         if best_score == nil || score > best_score
           best_moves = []
@@ -71,19 +86,27 @@ class GameHistory < ActiveRecord::Base
 
   :private
 
-  def self.compute_score(wins, losses, draws)
+  def self.compute_score(wins, losses, draws, enemy_wins, enemy_losses, enemy_draws)
+
+    own_score   = compute_score_helper(wins, losses, draws)
+    enemy_score = compute_score_helper(enemy_wins, enemy_losses, enemy_draws)
+
+    return (3 * own_score + 2 * enemy_score) / 5.0
+  end
+
+  def self.compute_score_helper(wins, losses, draws)
     total = wins + losses + draws
     return 0.1 if total == 0
 
     if wins > losses
-      wins += Math::log(total)
+      wins += Math::log(wins)
       total = wins + losses + draws
     elsif wins < losses
-      losses += Math::log(total)
+      losses += Math::log(losses)
       total = wins + losses + draws
     end
 
-    base_score = (5 * wins + 3 * draws) / (8 * total).to_f
+    return (5 * wins + 3 * draws) / (8 * total).to_f
   end
 
 end
