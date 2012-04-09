fcdb47ef234785edd5a791a461a0488504275bbb
Philipp Classen <philipp.classen@gmx.net>
2012-04-09 04:50:48 +0200

    Added performance test

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -9,4 +9,14 @@ class BrowsingTest < ActionDispatch::PerformanceTest
   def test_homepage
     get '/'
   end
+
+  def test_first_move_on_6_6_row4_board
+    post '/main_board/start_game', :board_size => 6, :row_size => 4, :first_move => 'x'
+  end
+
+  def test_first_move_on_10_10_row6_board
+    return if MAX_BOARD_SIZE < 10
+    post '/main_board/start_game', :board_size => 10, :row_size => 6, :first_move => 'x'
+  end
+
 end
